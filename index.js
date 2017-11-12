b78ad0f7498f8c1dad0152a3bd3718da3ae8a75e
evaparicio Evelyn <ev.aparicio.c@gmail.com>
2017-11-12 00:16:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 let currentUser = "Erendirani";
 const welcomeMessage= `Welcome to Flatbook, ${currentUser}!`
-const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
